13a463da6d5640ebba8b25c9f4af998d622e9594
Yu-jinKim <yujin.kim@hotmail.fr>
2020-06-24 14:58:25 +0100

    break script apart

---
+++ hgnc_queries/api.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+import json
+
+URL = "http://rest.genenames.org"
+
+
+def get_api_response(full_url):
+    """ send request to HGNC and get json response """
+
+    try:
+        response = requests.get(
+            full_url,
+            headers={"Accept": "application/json"}
+        )
+    except Exception as e:
+        print("Something wrong: {}".format(e))
+        sys.exit(-1)
+    else:
+        data = json.loads(response.content.decode("utf-8"))
+
+    return data
