39f0a48fbd4b827d3cdeb3c378b8f5ed4f9cc1ea
Yu-jinKim <yujin.kim@hotmail.fr>
2020-07-02 15:29:56 +0100

    new imports

--- hgnc_queries/__init__.py
+++ hgnc_queries/__init__.py
@@ -1,6 +1,10 @@
+from .api import get_api_response, URL
+from .conversion import (
+    convert_ensembl2refseq, convert_refseq2ensembl
+)
 from .queries import (
     get_new_symbol, get_gene_starting_with, get_alias,
     get_main_symbol, get_prev_symbol, get_id,
-    get_symbol_from_id, get_hgnc_symbol
+    get_symbol_from_id, get_hgnc_symbol,
+    get_ensembl, get_refseq
 )
-from .api import get_api_response
