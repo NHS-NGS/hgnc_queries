065f725e8f7709a4b3bcc3595c24330ed49b687f
Yu-jinKim <yujin.kim@hotmail.fr>
2020-07-02 15:31:14 +0100

    added tests for new functions

--- test/test_hgnc_queries.py
+++ test/test_hgnc_queries.py
@@ -1,18 +1,25 @@
 import pytest
-import sys
 
-from hgnc_queries import queries
+from hgnc_queries.conversion import (
+    convert_ensembl2refseq, convert_refseq2ensembl
+)
+from hgnc_queries.queries import (
+    get_new_symbol, get_gene_starting_with, get_alias,
+    get_main_symbol, get_prev_symbol, get_id,
+    get_symbol_from_id, get_hgnc_symbol,
+    get_ensembl, get_refseq
+)
 
 
 def test_get_new_symbol_no_res():
     assert(
-        queries.get_new_symbol("BRCA1P", False) is None
+        get_new_symbol("BRCA1P", False) is None
     )
 
 
 def test_get_new_symbol_one_res():
     assert(
-        queries.get_new_symbol("RN5S49", False) == "RNA5SP49"
+        get_new_symbol("RN5S49", False) == "RNA5SP49"
     )
 
 
@@ -21,21 +28,21 @@ def test_get_new_symbol_one_res():
 
 def test_get_gene_starting_with_no_res():
     assert(
-        queries.get_gene_starting_with("AIFJAEIGJI", False) is None
+        get_gene_starting_with("AIFJAEIGJI", False) is None
     )
 
 
 def test_get_gene_starting_with_single_res():
     truth = ["BRCA1P1"]
     assert(
-        queries.get_gene_starting_with("BRCA1P", False) == truth
+        get_gene_starting_with("BRCA1P", False) == truth
     )
 
 
 def test_get_gene_starting_with_multiple_res():
     truth_list = ["BRCA1", "BRCA1P1", "BRCA2", "BRCA3"]
     assert(
-        queries.get_gene_starting_with("BRCA", False) == truth_list
+        get_gene_starting_with("BRCA", False) == truth_list
     )
 
 
@@ -44,21 +51,21 @@ def test_get_gene_starting_with_multiple_res():
 
 def test_get_alias_no_alias():
     assert(
-        queries.get_alias("CARD9", False) is None
+        get_alias("CARD9", False) is None
     )
 
 
 def test_get_alias_one_alias():
     truth = ["DA9"]
     assert(
-        queries.get_alias("FBN2", False) == truth
+        get_alias("FBN2", False) == truth
     )
 
 
 def test_get_alias_multiple_aliases():
     truth = ["KIAA1235", "ELD/OSA1", "p250R", "BAF250b", "DAN15", "6A3-5"]
     assert(
-        queries.get_alias("ARID1B", False) == truth
+        get_alias("ARID1B", False) == truth
     )
 
 
@@ -67,14 +74,14 @@ def test_get_alias_multiple_aliases():
 
 def test_get_main_symbol_no_res():
     assert(
-        queries.get_main_symbol("CARD9", False) is None
+        get_main_symbol("CARD9", False) is None
     )
 
 
 def test_get_main_symbol_res():
     truth = "BRAF"
     assert(
-        queries.get_main_symbol("BRAF1", False) == truth
+        get_main_symbol("BRAF1", False) == truth
     )
 
 
@@ -83,21 +90,21 @@ def test_get_main_symbol_res():
 
 def test_get_prev_symbol_no_res():
     assert(
-        queries.get_prev_symbol("No exist", False) is None
+        get_prev_symbol("No exist", False) is None
     )
 
 
 def test_get_prev_symbol_one_res():
     truth = ["CCA"]
     assert(
-        queries.get_prev_symbol("FBN2", False) == truth
+        get_prev_symbol("FBN2", False) == truth
     )
 
 
 def test_get_prev_symbol_multiple_res():
     truth = ["SEDC", "AOM"]
     assert(
-        queries.get_prev_symbol("COL2A1", False) == truth
+        get_prev_symbol("COL2A1", False) == truth
     )
 
 
@@ -106,14 +113,14 @@ def test_get_prev_symbol_multiple_res():
 
 def test_get_id_no_res():
     assert(
-        queries.get_id("No exist", False) is None
+        get_id("No exist", False) is None
     )
 
 
 def test_get_id_one_res():
     truth = "HGNC:1097"
     assert(
-        queries.get_id("BRAF", False) == truth
+        get_id("BRAF", False) == truth
     )
 
 
@@ -122,20 +129,20 @@ def test_get_id_one_res():
 
 def test_get_symbol_from_id_not_ID():
     assert(
-        queries.get_symbol_from_id("No exist", False) is None
+        get_symbol_from_id("No exist", False) is None
     )
 
 
 def test_get_symbol_from_id_no_res():
     assert(
-        queries.get_symbol_from_id("54974894564156", False) is None
+        get_symbol_from_id("54974894564156", False) is None
     )
 
 
 def test_get_symbol_from_id_one_res():
     truth = "BRAF"
     assert(
-        queries.get_symbol_from_id("1097", False) == truth
+        get_symbol_from_id("1097", False) == truth
     )
 
 
@@ -144,17 +151,89 @@ def test_get_symbol_from_id_one_res():
 
 def test_get_hgnc_symbol_none():
     assert(
-        queries.get_hgnc_symbol("BRCA") is None
+        get_hgnc_symbol("BRCA") is None
     )
 
 
 def test_get_hgnc_symbol_use_new():
     assert(
-        queries.get_hgnc_symbol("RN5S49") == "RNA5SP49"
+        get_hgnc_symbol("RN5S49") == "RNA5SP49"
     )
 
 
 def test_get_hgnc_symbol_use_main():
     assert(
-        queries.get_hgnc_symbol("BRAF1") == "BRAF"
+        get_hgnc_symbol("BRAF1") == "BRAF"
+    )
+
+
+###########################################################################
+
+
+def test_convert_refseq2ensembl_wrong_id():
+    assert(
+        convert_refseq2ensembl("Not_ENSG") is None
+    )
+
+
+def test_convert_refseq2ensembl_no_res():
+    assert(
+        convert_refseq2ensembl("ENSG09184875872") is None
+    )
+
+
+def test_convert_refseq2ensembl_res():
+    assert(
+        convert_refseq2ensembl("NM_007294", False) == "ENSG00000012048"
+    )
+
+
+###########################################################################
+
+
+def test_convert_ensembl2refseq_wrong_id():
+    assert(
+        convert_ensembl2refseq("Not_NM") is None
+    )
+
+
+def test_convert_ensembl2refseq_no_res():
+    assert(
+        convert_ensembl2refseq("NM_9189483857") is None
+    )
+
+
+def test_convert_ensembl2refseq_res():
+    assert(
+        convert_ensembl2refseq("ENSG00000012048") == ['NM_007294']
+    )
+
+
+###########################################################################
+
+
+def test_get_refseq_wrong_id():
+    assert(
+        get_refseq("thingy") is None
+    )
+
+
+def test_get_refseq_res():
+    assert(
+        get_refseq("BRCA1") == ['NM_007294']
+    )
+
+
+###########################################################################
+
+
+def test_get_ensembl_wrong_id():
+    assert(
+        get_ensembl("thingy") is None
+    )
+
+
+def test_get_ensembl_res():
+    assert(
+        get_ensembl("BRCA1") == "ENSG00000012048"
     )
